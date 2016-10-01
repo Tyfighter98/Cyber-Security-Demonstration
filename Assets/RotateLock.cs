39a1a1ed7e8a37caeeb19e610cada863134d5235
Tyler Warren <tylerdwarren98@gmail.com>
2016-10-01 01:40:23 -0700

    Inital Lock Working

---
+++ Assets/RotateLock.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RotateLock : MonoBehaviour {
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        rotate(5);
+	}
+
+    void rotate(int numOfCharacters){
+        float rotation = 360 / numOfCharacters;
+        if (Input.GetKeyDown(KeyCode.LeftArrow)) {
+            transform.Rotate(0, 0, -rotation);
+        }
+
+        if (Input.GetKeyDown(KeyCode.RightArrow)) {
+            transform.Rotate(0, 0, rotation);
+        }
+    }
+}
