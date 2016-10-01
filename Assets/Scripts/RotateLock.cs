0e17b3a751cf0e72e1da3dbdd381c94b0f1cca11
Nikita Samarin <n.samarin.s@gmail.com>
2016-10-01 03:18:28 -0700

    Add logic for detecting password

--- Assets/Scripts/RotateLock.cs
+++ Assets/Scripts/RotateLock.cs
@@ -4,28 +4,54 @@ using System.Collections;
 public class RotateLock : MonoBehaviour {
 
 
-	// Use this for initialization
-	void Start () {
-	    
+    // Use this for initialization
+    int numberOfChars = 5;
+    char[] currentChar = {'a', 'b', 'c', 'd', 'e'};
+    int currentIndex;
+    string pwd;
+    
+    void Start () {
+        //int pwdLength = 1;
+        currentIndex = 0;
+        pwd = getPwd();
+        Debug.Log("Current pass is: " + pwd);
+    }
+
+    // Update is called once per frame
+    void Update () {
+        rotate(numberOfChars);
 	}
-	
-	// Update is called once per frame
-	void Update () {
-        rotate(5);
-	}
+
+    string getPwd() {
+        int fstCharAscii = (int)'a';
+        int sndCharAscii = fstCharAscii + numberOfChars;
+        char pwdChar = ((char)Random.Range(fstCharAscii, sndCharAscii));
+        return pwdChar.ToString();
+    }
 
     void rotate(int numOfCharacters){
         float rotation = 360 / numOfCharacters;
         if (Input.GetKeyDown(KeyCode.LeftArrow)) {
             transform.Rotate(0, 0, -rotation);
+            if (currentIndex == 0)
+                currentIndex = 5;
+            else
+                currentIndex--;
         }
 
         if (Input.GetKeyDown(KeyCode.RightArrow)) {
             transform.Rotate(0, 0, rotation);
+            if (currentIndex == 4)
+                currentIndex = 0;
+            else
+                currentIndex++;
         }
 
-        if (Input.GetKeyDown(KeyCode.KeypadEnter)) {
-            
+        if (Input.GetKeyDown("return")) {
+            if (pwd.Equals(currentChar[currentIndex].ToString()))
+                Debug.Log("Nice!");
+            else
+                Debug.Log("You Suck!");
         }
     }
 }
